5b0f4d769ea877a2c74ae01622cfb56ca75c12e0
wanghuafeng <wanghuafengc@163.com>
2015-12-21 08:47:58 +0800

    gevent 多线程压力测试

--- genent_crawl.py
+++ genent_crawl.py
@@ -8,14 +8,27 @@ import gevent.monkey
 
 gevent.monkey.patch_socket()
 
-thread_count = 4000  #指定同一时刻并发量
+thread_count = 100  #指定同一时刻并发量
 
-url = 'http://10.0.2.53:9091/Tasker?Action=0&TemplateID=200000'
+url = 'http://192.168.0.144:9081/IBANK'
 
+# cookie_dic = {
+#     '?BANK_TYPE':'60',
+#     'CARD_NO':'120105198907310623',
+#     'PWD':'Rng360',
+#     'MAIN_CARD':'4367420********7791',
+# }
+# r = requests.get(url, cookies=cookie_dic)
+
+s = requests.Session()  #注意cookie设置时，value必须为string基础类型
+s.cookies['?BANK_TYPE'] = '60'
+s.cookies['CARD_NO'] = '120105198907310623'
+s.cookies['PWD'] = 'Rng360'
+s.cookies['MAIN_CARD'] = '4367420********7791'
 
 def read(url):
     try:
-        context = requests.post(url)
+        context = s.get(url)
         print context
     except Exception:
         print "load %s failure." %url
@@ -28,9 +41,10 @@ def concuyRead():
         threads.append(gevent.spawn(read, url))
     gevent.joinall(threads)
     end = time.time()
-    print "Elapsed Time : %d" %(end-start)
+    print "consume time: %d" %(end-start)
 
 if __name__ == '__main__':
-    for i in range(1, 20):
-        concuyRead()
-        time.sleep(1)
+    concuyRead()
+    # for i in range(1, 20):
+    #     concuyRead()
+    #     time.sleep(1)
