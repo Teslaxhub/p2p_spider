4e7fe186a79e07d20e68d7e8d0adbe18437ce9d7
wanghuafeng <wanghuafengc@163.com>
2016-04-15 09:21:32 +0800

    ppd add analyse method

--- ppdai.py
+++ ppdai.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import re
 import requests
+from collections import Counter
 from bs4 import BeautifulSoup
 
 ss = requests.session()
@@ -28,9 +29,7 @@ def get_page_no():
     except BaseException:
         print '黑名单总页数抓取失败'
     return page_no
-
 id_list = [item['listingid']  for item in soup.find_all('tr', 'blacklistdetail dn')]
-
 def get_credit_Rating(user_id):
     '''根据用户id，获取评级参数'''
     detail_url = 'http://invest.ppdai.com/loan/info?id=' + user_id
@@ -40,9 +39,22 @@ def get_credit_Rating(user_id):
     # user_desc = creditRatingSoup.find('div', class_='lendDetailTab_tabContent').find('p').text
     user_desc = creditRatingSoup.find('span', attrs={'tt':re.compile('\d*')}).text
     return user_desc, creditRating
-    # print  user_desc
-
-    # return creditRating
-# get_credit_Rating('')
-for user_id in id_list:
-    print '\t'.join(get_credit_Rating(user_id))
+if __name__ == '__main__':
+    page_no = get_page_no()
+    credit_rate_list = []
+    for index in range(1, int(page_no)+1):
+        print '----------------当前第:%s页------------------' % index
+        black_list_page_url = 'http://invest.ppdai.com/account/blacklist?PageIndex=%s&IsCalendarRequest=0'%index
+        black_list_html = ss.get(black_list_page_url).content
+        soup = BeautifulSoup(black_list_html)
+        id_list = [item['listingid']  for item in soup.find_all('tr', 'blacklistdetail dn')]
+        for user_id in id_list:
+            user_desc_crdit_rate_tuple = get_credit_Rating(user_id)
+            print '\t'.join(user_desc_crdit_rate_tuple)
+            credit_rate_list.append(user_desc_crdit_rate_tuple[-1])
+    rating_counter = Counter(credit_rate_list)
+    val_sum = sum(rating_counter.values())
+    print rating_counter
+    print 'total overDuePerson count: %s' % val_sum
+    for rating in rating_counter:
+        print rating, float(rating_counter[rating])/val_sum
